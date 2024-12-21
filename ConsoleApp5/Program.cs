531b5679295dfef227e103cb38fd42f4373ca7e8
dell <dell@DESKTOP-26ON3SK>
2024-12-21 19:13:36 +0200

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.ComponentModel;
+using System.Xml.Linq;
+
+class Program
+{ static void Main()
+    {
+        //#region  Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
+        //Console.Write("Enter a number: ");
+        //int number = int.Parse(Console.ReadLine());
+        //if (number % 3 == 0 && number % 4 == 0)
+        //{
+        //    Console.WriteLine("Yes");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("No");
+        //}
+        //#endregion
+
+        //#region 2- Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.
+        //Console.WriteLine("enter a integer :");
+        //int num=int.Parse(Console.ReadLine());
+        //if (num < 0)
+        //{
+        //    Console.WriteLine("negative");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("positive");
+        //}
+        //#endregion
+        //#region 3 - Write a program that takes 3 integers from the user then prints the max element and the min element.
+        //Console.Write("Enter the first integer: ");
+        //int num1 = int.Parse(Console.ReadLine());
+
+        //Console.Write("Enter the second integer: ");
+        //int num2 = int.Parse(Console.ReadLine());
+
+        //Console.Write("Enter the third integer: ");
+        //int num3 = int.Parse(Console.ReadLine());
+        //int maxElement = num1;
+        //if (num2 > maxElement) maxElement = num2;
+        //if (num3 > maxElement) maxElement = num3;
+        //int minElement = num1;
+        //if (num2 < minElement) minElement = num2;
+        //if (num3 < minElement) minElement = num3;
+        //Console.WriteLine(maxElement);
+        //Console.WriteLine(minElement);
+
+        //#endregion
+        //#region 4- Write a program that allows the user to insert an integer number then check If a number is even or odd.
+        //Console.WriteLine("enter anumber");
+        //int num = int.Parse(Console.ReadLine());
+        //if(num%2 == 0) {
+        //    Console.WriteLine("even");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("odd");
+        //}
+        //#endregion
+        //#region 5- Write a program that takes character from the user then if it is a vowel chars (a,e,I,o,u) then print (vowel) otherwise print (consonant).
+        //Console.WriteLine("enter character:");
+        //char ch=char.Parse(Console.ReadLine());
+        //if(ch == 'a' || ch == 'o' || ch == 'e' || ch == 'i' || ch == 'u')
+        //{
+        //    Console.WriteLine("vowel");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Consonant");
+        //}
+        ////#endregion
+        //#region 6- Write a program that allows the user to insert an integer then print a multiplication table up to 12.
+        //Console.WriteLine("enter number");
+        //int num=int.Parse(Console.ReadLine());
+        //for (int i = 1; i <= 12; i++)
+        //{
+        //    Console.WriteLine(num*i);
+        //}
+        //#endregion
+        //        #region 7- Write a program that allows to user to insert number then print all even numbers between 1 to this number
+        //        Console.WriteLine("enter a number:");
+        //        int num=int.Parse(Console.ReadLine());
+        //        for(int i = 2; i < num; i += 2)
+        //        {
+        //            Console.WriteLine(i);
+        //        }
+
+        //#endregion
+        //        #region 8- Write a program that takes two integers then prints the power.
+        //        Console.Write("enter the number ");
+        //        int n = int.Parse(Console.ReadLine());
+        //        Console.Write("enter the power");
+        //        int p = int.Parse(Console.ReadLine());
+        //        int result = 1;
+        //        for (int i2 = 1; i2 <= p; i2++)
+        //        {
+        //            result *= n;
+        //        }
+        //        Console.WriteLine(" the result is " + result);
+        //#endregion
+        //#region
+        //Console.Write("Enter the month number ");
+        //int month = int.Parse(Console.ReadLine());
+        //int days = 0;
+        //switch (month)
+        //{
+        //    case 1:
+        //    case 3:
+        //    case 5:
+        //    case 7:
+        //    case 8:
+        //    case 10:
+        //    case 12:
+        //        days = 31;
+        //        break;
+        //    case 4:
+        //    case 6:
+        //    case 9:
+        //    case 11:
+        //        days = 30;
+        //        break;
+        //    case 2:
+        //        days = 28;
+        //        break;
+
+        //    default:
+        //        Console.WriteLine("Invalid month number");
+        //        return;
+        //}
+        //Console.WriteLine($"Days in month {month}: {days}");
+        //#endregion
+        //#region q)10
+        //Console.WriteLine("enter hours:");
+        //    double hourse=double.Parse(Console.ReadLine());
+        //if(hourse > 2 && hourse <= 3)
+        //{
+        //    Console.WriteLine("Highly Efficient");
+        //}
+        //else if (hourse > 3 && hourse <= 4)
+        //{
+        //    Console.WriteLine("Increase your speed");
+        //}
+        //else if(hourse > 4 && hourse <= 5) { 
+        //    Console.WriteLine("Provide training to increase speed"); }
+        //else if(hourse > 5) { 
+        //    Console.WriteLine("Required to leave the company");
+        //}
+        //else
+        //{
+        //    Console.Write("invalid");
+        //}
+
+        //#endregion
+
+    }
+}
